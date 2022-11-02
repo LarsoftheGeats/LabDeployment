7f3c5ae6c261d742ac29d1d384b277c1b323b72b
Jonathan Nielsen <larsnielsenj@gmail.com>
2022-11-02 15:07:30 -0600

    fixed

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const {home} = require("./controller/pageCtrl.js")
+const {home, cssServe} = require("./controller/pageCtrl.js")
 
 app.get("/", home)
 
