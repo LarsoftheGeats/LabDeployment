de8f5fecd0409c9dc78be0716543f62723d7c6f3
Jonathan Nielsen <larsnielsenj@gmail.com>
2022-11-02 15:00:50 -0600

    may have fixed it

--- server/controller/pageCtrl.js
+++ server/controller/pageCtrl.js
@@ -5,9 +5,9 @@ module.exports = {
         res.sendFile(path.join( __dirname, "../../index.html"))
     },
 
-    scriptServe: (req, res) => {
-        res.sendFile(path.join(__dirname, './public/main.js'))
-    },
+    // scriptServe: (req, res) => {
+    //     res.sendFile(path.join(__dirname, './public/main.js'))
+    // },
 
     cssServe: (req,res) => {
         res.sendFile(path.join(__dirname, '../../styling.css'))
